2e70500401f7969ff3aa415d47b78d97aabce4cf
Zach <zachmvm@gmail.com>
2024-01-16 23:24:51 -0500

    add logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,6 +13,10 @@ const Navbar = () => {
                 height={27}
                 alt="logo"
             />
+
+            <p className="nav-logo">
+              Price<span className="text-primary">Wise</span>
+            </p>
             </Link>
         </nav>
     </header>
