b670e370d266a4d9158fa7eb6b4154b70dfd42f3
Zach <zachmvm@gmail.com>
2024-01-21 19:35:51 -0500

    add search bar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,10 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
+const navIcons = [
+  { src: '/assets/icons/search.svg', alt: 'search'}
+]
+
 const Navbar = () => {
   return (
     <header className="w-full">
@@ -18,6 +22,22 @@ const Navbar = () => {
               Price<span className="text-primary">Wise</span>
             </p>
             </Link>
+
+            <div className="flex items -center gap-5">
+              {navIcons.map((icon) => (
+                <Image
+                key={icon.alt}
+                src={icon.src}
+                alt={icon.alt}
+                width={28}
+                height={28}
+                className="object-contain"
+                />
+              ))}
+
+            </div>
+
+
         </nav>
     </header>
   )
